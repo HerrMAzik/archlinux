306b0a1029222d1ef5738988fe413d664e38c125
Azat Mirzaianov <herr.mazik@gmail.com>
2020-04-02 18:31:19 +0400

    Update config.bash

--- config.bash
+++ config.bash
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-sudo systemctl enable --now NetworkManager.service
-sudo systemctl disable --now NetworkManager-wait-online.service
-sudo systemctl enable --now dnscrypt-proxy.service
+sudo sh <<EOF
+systemctl enable --now NetworkManager.service
+systemctl disable --now NetworkManager-wait-online.service
+systemctl enable --now dnscrypt-proxy.service
+systemctl enable --now fstrim.timer
+EOF
 
 nmtui
 
-sudo pacman --needed --noconfirm -Syyuu unzip zip p7zip pigz pbzip2 xz python
+sudo pacman --needed --noconfirm -Syyuu unzip zip p7zip pigz pbzip2 xz python git
 
-# figure out how to enable multilib in /etc/pacman.conf using cli
+git clone https://github.com/HerrMAzik/arch-setup.git conf
+cd conf
+sudo python config.py
+cd ..
+rm -rf conf
 
 sudo sh <<EOF
 cat <<EOF2 > /etc/modprobe.d/blacklist.conf
@@ -32,6 +39,27 @@ sudo pacman --needed --noconfirm -S git gcc gdb cmake
 # yay
 curl -L https://git.io/Jvd0P | bash
 
-# figure out how to adjust the /etc/makepkg.conf
-
 yay -S polybar --needed --noconfirm
+
+sudo sh <<EOF
+cp -f /etc/systemd/swap.conf /etc/systemd/swap.conf.bak
+sed -i 's/^\(swapfc_force_preallocated=\)[[:digit:]]\(.*\)$/\11\2/' /etc/systemd/swap.conf
+sed -i 's/\(swapfc_enabled=\)[[:digit:]]\(.*\)$/\11\2/' /etc/systemd/swap.conf
+systemctl enable systemd-swap.service
+EOF
+
+echo 'SSH_AUTH_SOCK DEFAULT="${XDG_RUNTIME_DIR}/ssh-agent.socket"' > $HOME/.pam_environment
+mkdir -p $HOME/.config/systemd/user
+cat <<EOF > $HOME/.config/systemd/user/ssh-agent.service
+[Unit]
+Description=SSH key agent
+
+[Service]
+Type=simple
+Environment=SSH_AUTH_SOCK=%t/ssh-agent.socket
+ExecStart=/usr/bin/ssh-agent -D -a \$SSH_AUTH_SOCK
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl --user enable --now ssh-agent.service
