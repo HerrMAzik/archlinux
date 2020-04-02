b404963cd9663a2c88f3eb7146eb75e239806c84
Azat Mirzaianov <herr.mazik@gmail.com>
2020-04-02 12:20:29 +0400

    Update config.bash

--- config.bash
+++ config.bash
@@ -9,26 +9,22 @@ nmtui
 # figure out how to enable multilib in /etc/pacman.conf using cli
 sudo pacman --noconfirm -Syyuu
 
-sudo pacman --noconfirm -S zsh
-
-sudo useradd -m -g users -G audio,video,power,storage,wheel -s /bin/zsh azat
-sudo passwd azat
-
 sudo sh <<EOF
 cat <<EOF2 > /etc/modprobe.d/blacklist.conf
 blacklist bluetooth
 blacklist btusb
 blacklist uvcvideo
+blacklist nouveau
 EOF2
 echo 'vm.swappiness = 10' > /etc/sysctl.d/90-swappiness.conf
 EOF
 
 sudo pacman --needed --noconfirm -S alsa-utils pulseaudio-alsa pulsemixer
-sudo pacman --needed --noconfirm -S xorg-server xorg-xprop picom bspwm sxhkd
+sudo pacman --needed --noconfirm -S xorg-server xorg-xprop bspwm sxhkd xdg-user-dirs
 sudo pacman --needed --noconfirm -S mpv
 sudo pacman --needed --noconfirm -S ttf-jetbrains-mono
 sudo pacman --needed --noconfirm -S ranger pass oath-toolkit mc curl wget
-sudo pacman --needed --noconfirm -S exa ripgrep fd bat
+sudo pacman --needed --noconfirm -S exa ripgrep fd bat alacritty
 sudo pacman --needed --noconfirm -S systemd-swap redshift
 sudo pacman --needed --noconfirm -S unzip zip p7zip pigz pbzip2 xz
 sudo pacman --needed --noconfirm -S git gcc gdb cmake
