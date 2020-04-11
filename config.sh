9497ee5eddc11fe21253dd5eb694a1b44fcf32b4
Azat Mirzaianov <herr.mazik@gmail.com>
2020-04-11 15:33:22 +0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -67,6 +67,7 @@ noto-fonts-extra
 noto-fonts-cjk
 noto-fonts-emoji
 ttf-jetbrains-mono
+ttf-font-awesome
 
 alsa-utils
 pulseaudio-alsa
@@ -198,7 +199,78 @@ sudo mkdir -p /etc/sddm.conf.d
 sudo systemctl enable sddm.service
 
 mkdir -p $XDG_CONFIG_HOME/picom
-cp /etc/xdg/picom.conf $XDG_CONFIG_HOME/picom/
+cat <<EOF > $XDG_CONFIG_HOME/picom/picom.conf
+backend = "glx";
+glx-no-stencil = true;
+glx-copy-from-front = false;
+shadow = true;
+shadow-radius = 5;
+shadow-offset-x = -5;
+shadow-offset-y = -5;
+shadow-opacity = 0.5;
+shadow-exclude = [
+    "! name~=''",
+    "name = 'Notification'",
+    "name = 'Plank'",
+    "name = 'Docky'",
+    "name = 'Kupfer'",
+    "name = 'xfce4-notifyd'",
+    "name *= 'VLC'",
+    "name *= 'compton'",
+    "name *= 'picom'",
+    "name *= 'Chromium'",
+    "name *= 'Chrome'",
+    "class_g = 'Firefox' && argb",
+    "class_g = 'Conky'",
+    "class_g = 'Kupfer'",
+    "class_g = 'Synapse'",
+    "class_g ?= 'Notify-osd'",
+    "class_g ?= 'Cairo-dock'",
+    "class_g ?= 'Xfce4-notifyd'",
+    "class_g ?= 'Xfce4-power-manager'",
+    "_GTK_FRAME_EXTENTS@:c",
+    "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+];
+shadow-ignore-shaped = false;
+inactive-opacity = 1;
+active-opacity = 1;
+frame-opacity = 1;
+inactive-opacity-override = false;
+blur-background-fixed = false;
+blur-background-exclude = [
+    "window_type = 'dock'",
+    "window_type = 'desktop'"
+];
+fading = true;
+fade-delta = 4;
+fade-in-step = 0.03;
+fade-out-step = 0.03;
+fade-exclude = [ ];
+mark-wmwin-focused = true;
+mark-ovredir-focused = true;
+use-ewmh-active-win = true;
+detect-rounded-corners = true;
+detect-client-opacity = true;
+refresh-rate = 0;
+vsync = true;
+dbe = false;
+unredir-if-possible = false;
+focus-exclude = [ ];
+detect-transient = true;
+detect-client-leader = true;
+wintypes:
+{
+    tooltip =
+    {
+        fade = true;
+        shadow = false;
+        opacity = 0.85;
+        focus = true;
+    };
+};
+xrender-sync-fence = true;
+
+EOF
 
 mkdir -p $XDG_CONFIG_HOME/sxhkd
 cat <<EOF > $XDG_CONFIG_HOME/sxhkd/sxhkdrc
@@ -298,6 +370,14 @@ super + {Up, Down}
 
 EOF
 
+mkdir -p $XDG_CONFIG_HOME/polybar
+cat <<EOF > $XDG_CONFIG_HOME/polybar/launch.sh
+killall -q polybar
+echo "----------" | tee -a /tmp/polybar1.log
+polybar bar1 >> /tmp/polybar1.log 2>&1 &
+EOF
+chmod +x $XDG_CONFIG_HOME/polybar/launch.sh
+
 mkdir -p $XDG_CONFIG_HOME/bspwm
 cat <<EOF > $XDG_CONFIG_HOME/bspwm/bspwmrc
 #!/bin/sh
@@ -306,6 +386,7 @@ sxhkd &
 feh --no-fehbg --bg-scale "\$HOME/repo/arch-setup/lancer.jpg"
 setxkbmap -model pc105 -layout us,ru -option grp:toggle
 xorg-xsetroot -cursor_name left_ptr
+#$XDG_CONFIG_HOME/polybar/launch.sh
 
 ### Gaps ###
 bspc config top_padding        2
