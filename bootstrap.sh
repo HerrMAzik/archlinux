ec0130e7b540f807ea9162309ec39dc36cf8a452
Azat Mirzaianov <herr.mazik@gmail.com>
2020-06-06 15:59:39 +0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-bootstrapper_dialog() {
-    DIALOG_RESULT=$(dialog --clear --stdout --backtitle "Arch bootstrapper" --no-shadow "$@" 2>/dev/null)
-}
-
 WARN_END=" (cannot be omitted ot empty)"
 
-OLDIFS=$IFS
-IFS=":"
-DISKS=$(lsblk --nodeps -n -o path,size -I8,259 | awk '{print $1":"$2}' ORS=':')
+bootstrapper_dialog() {
+    DIALOG_RESULT=$(dialog --clear --stdout --backtitle "Arch bootstrapper" --no-shadow --no-cancel "$@" 2>/dev/null)
+}
+
+MODES="1 BIOS"
+[ -d /sys/firmware/efi ] && MODES="$MODES 2 UEFI"
+bootstrapper_dialog --title "MODE" --menu "Please select a boot mode from the following list" 13 70 3 $MODES
+[ $? -ne 0 ] || [ -z $DIALOG_RESULT ] && MODE=1 || MODE=$DIALOG_RESULT
+
+DISKS=$(lsblk --nodeps -n -o path,size -I8,259 | awk '{print $1" "$2}' ORS=' ')
 bootstrapper_dialog --title "Device" --menu "Please select a device from the following list to use for Linux installation." 13 70 3 $DISKS
 [ $? -ne 0 ] || [ -z $DIALOG_RESULT ] && echo 'No device selected' && exit 0
-IFS=$OLDIFS
 DEVICE=$DIALOG_RESULT
 
 bootstrapper_dialog --title "WARNING" --msgbox "This script will NUKE $DEVICE from orbit.\nPress <Enter> to continue or <Esc> to cancel.\n" 6 60
@@ -48,11 +50,24 @@ USER_PASSWORD="$DIALOG_RESULT"
 
 reset
 
+timedatectl set-ntp true
 export LANG=en_US.UTF-8
 
-echo ';' | sfdisk $DEVICE
-yes | mkfs.ext4 -L system "${DEVICE}1"
-mount "${DEVICE}1" /mnt
+if [ $MODE -eq 1 ]; then
+    echo ';' | sfdisk $DEVICE
+    yes | mkfs.ext4 -L system "${DEVICE}1"
+    mount "${DEVICE}1" /mnt
+else
+    sgdisk --zap-all $DEVICE
+    sgdisk -o $DEVICE
+    sgdisk -n 1:0:+128M -t 1:ef00 $DEVICE
+    sgdisk -N 2 -t 2:8300 $DEVICE
+
+    mkfs.fat -F32 "${DEVICE}1"
+    mkfs.ext4 -L system "${DEVICE}2"
+
+    mount "${DEVICE}2" /mnt
+fi
 
 sed -n "/yandex/p" -i /etc/pacman.d/mirrorlist
 yes '' | pacstrap /mnt base base-devel linux linux-firmware
@@ -70,6 +85,7 @@ export LANG=en_US.UTF-8
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=us' > /etc/vconsole.conf
 ln -s /usr/share/zoneinfo/Europe/Samara /etc/localtime
+hwclock --systohc
 cat << EOF2 > /etc/hosts
 127.0.0.1   localhost
 ::1         localhost ip6-localhost ip6-loopback
@@ -86,10 +102,21 @@ chown $USERNAME:users /home/$USERNAME/system.sh
 chmod 0700 /home/$USERNAME/system.sh
 EOF
 
-arch-chroot /mnt /bin/sh <<EOF
-pacman --noconfirm -S grub
-grub-install $DEVICE
-grub-mkconfig -o /boot/grub/grub.cfg
-EOF
+if [ $MODE -eq 1 ]; then
+    arch-chroot /mnt /bin/sh <<EOF
+    pacman --noconfirm -S grub
+    grub-install $DEVICE
+    grub-mkconfig -o /boot/grub/grub.cfg
+    EOF
+else
+    arch-chroot /mnt /bin/sh <<EOF
+    pacman --noconfirm -S grub efibootmgr
+    mkdir -p /efi
+    mount "${DEVICE}1" /efi
+    grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+    grub-mkconfig -o /boot/grub/grub.cfg
+    umount /efi
+    EOF
+fi
 
 umount /mnt
