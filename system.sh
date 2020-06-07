c225b0c26dd98333a26393af405ba04a987340f5
Azat Mirzaianov <herr.mazik@gmail.com>
2020-06-08 00:16:52 +0400

    Update system.sh

--- system.sh
+++ system.sh
@@ -37,6 +37,7 @@ pacman --needed --noconfirm -S noto-fonts noto-fonts-extra noto-fonts-cjk noto-f
 pacman --needed --noconfirm -S ttf-jetbrains-mono ttf-dejavu ttf-opensans
 pacman --needed --noconfirm -S xdg-user-dirs gnome-shell gdm gnome-terminal nautilus
 pacman --needed --noconfirm -S gnome-control-center gnome-tweaks evince eog file-roller
+pacman --needed --noconfirm -S gnome-calculator
 pacman --needed --noconfirm -S mpv youtube-dl firefox ncdu transmission-gtk
 pacman --needed --noconfirm -S pass oath-toolkit keepassxc keybase kbfs gnupg
 pacman --needed --noconfirm -S mc curl wget htop neovim jq expect
