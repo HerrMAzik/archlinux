11a6b61f7c3a25a4db4968b77da6502a6b538e55
Azat Mirzaianov <herr.mazik@gmail.com>
2020-09-06 21:33:08 +0400

    Delete java.sh

--- java.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-yay -S --needed --noconfirm jdk-openjdk openjdk-src openjdk-doc
-! type intellij-idea-ultimate-edition >/dev/null 2>&1 && yay -S --needed --noconfirm --removemake intellij-idea-ultimate-edition intellij-idea-ultimate-edition-jre
