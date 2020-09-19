5310cd95445844631b1e80d951d4cb9f26a15306
HerrMAzik <herr.mazik@gmail.com>
2020-09-19 15:06:01 +0400

    add keyserve for importing keys

--- build/gnupg.sh
+++ build/gnupg.sh
@@ -3,7 +3,7 @@
 build=$PWD
 
 # Werner Koch (gnupg)
-gpg --list-keys 249B39D24F25E3B6 || gpg --receive-keys 249B39D24F25E3B6
+gpg --list-keys 249B39D24F25E3B6 || gpg --keyserver=pool.sks-keyservers.net --receive-keys 249B39D24F25E3B6
 cd $HOME/build
 asp update gnupg
 asp export gnupg
