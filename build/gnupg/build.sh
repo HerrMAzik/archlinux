ac63f7c86686e0b6842b55a3204054db586a3f39
Azat Mirzaianov <herr.mazik@gmail.com>
2020-12-07 01:33:24 +0400

    Update build.sh

--- build/gnupg/build.sh
+++ build/gnupg/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 PKGVER=2.2.25
 PKGREL=1
@@ -11,9 +11,10 @@ asp update gnupg
 asp export gnupg
 cd gnupg
 
-rg "^pkgver${PKGVER}$" PKGBUILD >/dev/null && rg "^pkgrel=${PKGREL}$" PKGBUILD >/dev/null && echo 'Package not updated' && exit 0
+rg "^pkgver${PKGVER}$" PKGBUILD >/dev/null && rg "^pkgrel=${PKGREL}$" PKGBUILD >/dev/null && echo 'Package is up-to-date' && exit 0
 
 cp $build/gnupg/pinentry-title.patch ./
+cat PKGBUILD | sd --flags cms 'prepare\(\)(.+)./autogen.sh' 'prepare()${1}patch -p1 -i ../pinentry-title.patch\n./autogen.sh' | sd --flags cms 'source=\(([^\)]+)' 'source=(${1}\n\'pinentry-title.patch\'' | sd --flags cms 'sha256sums=\(([^\)]+)' 'sha256sums=(${1}\n\'13bc1103c3340478f9f3e72ef95f4024739200aa86226742e743b56a113b04ca\'' > 1 && mv 1 PKGBUILD
 makepkg --syncdeps --install --noconfirm --needed
 
-sed -i 's/#pinentry-title/pinentry-title/g' $HOME/.gnupg/gpg-agent.conf
+sd '^#pinentry-title' 'pinentry-title' $HOME/.gnupg/gpg-agent.conf
