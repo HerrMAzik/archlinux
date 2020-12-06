33fe53a608d0694b68919588a0957e01894e7f87
Azat Mirzaianov <herr.mazik@gmail.com>
2020-12-06 23:05:07 +0400

    Update and rename build/gnupg.sh to build/gnupg/build.sh

--- build/gnupg/build.sh
+++ build/gnupg/build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+PKGVER=2.2.25
+PKGREL=1
 build=$PWD
 
 # Werner Koch (gnupg)
 gpg --list-keys 528897B826403ADA || gpg --keyserver=pool.sks-keyservers.net --receive-keys 528897B826403ADA
-cd $HOME/build
+cd $HOME/build && rm -rf gnupg || echo 'old package folder removing failed' && exit -1
 asp update gnupg
 asp export gnupg
 cd gnupg
 
-cp $build/gnupg/* ./
+rg "^pkgver${PKGVER}$" PKGBUILD >/dev/null && rg "^pkgrel=${PKGREL}$" PKGBUILD >/dev/null && echo 'Package not updated' && exit 0
+
+cp $build/gnupg/pinentry-title.patch ./
 makepkg --syncdeps --install --noconfirm --needed
 
 sed -i 's/#pinentry-title/pinentry-title/g' $HOME/.gnupg/gpg-agent.conf
