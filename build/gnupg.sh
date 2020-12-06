320fcb958036c73dfe2014fa93abef568ba86f51
Azat Mirzaianov <herr.mazik@gmail.com>
2020-12-06 20:18:05 +0400

    Update gnupg.sh

--- build/gnupg.sh
+++ build/gnupg.sh
@@ -3,13 +3,13 @@
 build=$PWD
 
 # Werner Koch (gnupg)
-gpg --list-keys 249B39D24F25E3B6 || gpg --keyserver=pool.sks-keyservers.net --receive-keys 249B39D24F25E3B6
+gpg --list-keys 528897B826403ADA || gpg --keyserver=pool.sks-keyservers.net --receive-keys 528897B826403ADA
 cd $HOME/build
 asp update gnupg
 asp export gnupg
 cd gnupg
 
 cp $build/gnupg/* ./
-makepkg -s --noconfirm --needed
+makepkg --syncdeps --install --noconfirm --needed
 
 sed -i 's/#pinentry-title/pinentry-title/g' $HOME/.gnupg/gpg-agent.conf
