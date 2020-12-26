97ff648f3d2241b45687e8ac0b68343284f3e503
HerrMAzik <herr.mazik@gmail.com>
2020-12-26 15:58:47 +0400

    extract kdb file getting

--- 02_config.sh
+++ 02_config.sh
@@ -2,12 +2,12 @@
 
 sudo echo 'Preparing:'
 
-! type git >/dev/null && echo 'install git at first running system.sh' && exit -1
+! type git >/dev/null && echo 'install git at first by running system.sh' && exit -1
 test ! -d $HOME/repo/archlinux && echo 'run system.sh before configuring' && exit -1
 
 test -z $XDG_CONFIG_HOME && XDG_CONFIG_HOME="$HOME/.config"
 test -z $ARCHLINUX && ARCHLINUX=$HOME/repo/archlinux
-MAN_KDB="$HOME/repo/man.kdbx"
+SANCTUM_SANCTORUM="$HOME/repo/man.kdbx"
 
 rm -rf $HOME/.bashrc
 rm -rf $HOME/.bash_{logout,profile}
@@ -15,31 +15,8 @@ rm -rf $HOME/.bash_{logout,profile}
 mkdir -p $HOME/build
 mkdir -p $HOME/repo
 
-if [ ! -f $MAN_KDB ]; then
-    while : ; do
-        test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
-        link=$(echo 'jA0ECQMCItwerSoXDA3o0lYBs5LFklMXCSTWb9FFsTdqXcPlVoFAHK6q6dZc7OF04lhUcFiKpCgpUgSde+CuPhSqIcGzdD7dyizdgu4aA91oX+QdhN7KLdiJSp7YJ7QyQTbYY2Smaw==' | base64 --decode | gpg --decrypt --batch --quiet --passphrase "$passphrase")
-        [ $? -eq 0 ] && echo $link && break
-        unset passphrase
-    done
-
-    resp=$(curl -sSL "https://cloud-api.yandex.net/v1/disk/public/resources?public_key=$link")
-    error=$(echo $resp | jq -r .error)
-    [ "$error" != "null" ] && echo "Error: $error"
-    link=$(echo $resp | jq -r .file)
-    [ "$link" == "null" ] || [ $? -ne 0 ] && link=$(echo 'jA0ECQMCXOfwQMLRH93p0lkBXcQzC1SyWhccifyEn1QGeU7VS7Q7+aJLuI5iP7EiOBkKGaMteZ68aF6bbuVGjjZLw4L/BB3br6CK+4yF0/0nRREXoQyEee1AVoE1OaDG/kqq7oa/QFy3Kg==' | base64 --decode | gpg --decrypt --batch --quiet --passphrase "$passphrase")
-
-    curl -sSL --output /tmp/kdbx $link
-    echo "kdbx has been downloaded"
-
-    while : ; do
-        echo 'enter password for kdb archive:' && read -ers z
-        gpg --passphrase "$z" --batch --quiet --decrypt /tmp/kdbx | xz -d > $MAN_KDB
-        [ $? -eq 0 ] && break
-	rm -rf $MAN_KDB
-    done
-fi
-[ ! -f $MAN_KDB ] && echo 'KDBX is not ready' && exit -1
+. ./sanctum.sanctorum.sh
+[ ! -f $SANCTUM_SANCTORUM ] && echo 'KDBX is not ready' && exit -1
 
 while : ; do
     hash=$(test -f $HOME/.sanctum.sanctorum && sha512sum $HOME/.sanctum.sanctorum | awk '{ print $1 }' || echo 0)
@@ -53,7 +30,7 @@ if [ ! -f $HOME/.dots.secret ]; then
     while : ; do
         test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
 
-        yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $MAN_KDB dots.secret | base64 --decode | gpg --passphrase $passphrase --decrypt --batch --quiet --output $HOME/.dots.secret
+        yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM dots.secret | base64 --decode | gpg --passphrase $passphrase --decrypt --batch --quiet --output $HOME/.dots.secret
         [ $? -eq 0 ] && break
     done
     hash=$(test -f $HOME/.dots.secret && sha512sum $HOME/.dots.secret | awk '{ print $1 }' || echo 0)
@@ -76,8 +53,8 @@ sed -i 's/^pinentry-title/#pinentry-title/g' $HOME/.gnupg/gpg-agent.conf
 if [ ! -f $XDG_CONFIG_HOME/keybase/*.mpack ]; then
     test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
 
-    mpack_filename=$(yes $passphrase | keepassxc-cli show -q -a UserName -s -k $HOME/.sanctum.sanctorum $MAN_KDB Programs/Keybase/mpack)
-    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $MAN_KDB Programs/Keybase/mpack | base64 --decode > $XDG_CONFIG_HOME/keybase/$mpack_filename
+    mpack_filename=$(yes $passphrase | keepassxc-cli show -q -a UserName -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM Programs/Keybase/mpack)
+    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM Programs/Keybase/mpack | base64 --decode > $XDG_CONFIG_HOME/keybase/$mpack_filename
     chmod 0600 $XDG_CONFIG_HOME/keybase/$mpack_filename
 fi
 
@@ -87,9 +64,9 @@ systemctl --user enable --now ssh-agent.service
 if [ ! $(gpg --list-keys prime > /dev/null 2>&1) ]; then
     test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
 
-    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $MAN_KDB GPG/keys/private | gpg --pinentry-mode loopback --passphrase $(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $MAN_KDB GPG/gpg) --import
-    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $MAN_KDB GPG/keys/public | gpg --import
-    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $MAN_KDB GPG/keys/trust | awk NF | gpg --import-ownertrust
+    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM GPG/keys/private | gpg --pinentry-mode loopback --passphrase $(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM GPG/gpg) --import
+    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM GPG/keys/public | gpg --import
+    yes $passphrase | keepassxc-cli show -q -a Notes -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM GPG/keys/trust | awk NF | gpg --import-ownertrust
 fi
 
 sh -c "cd $ARCHLINUX; git remote set-url origin git@github.com:HerrMAzik/archlinux.git"
@@ -97,7 +74,7 @@ sh -c "cd $ARCHLINUX; git remote set-url origin git@github.com:HerrMAzik/archlin
 if [ ! -d $HOME/repo/pass ]; then
     test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
 
-    git clone https://HerrMAzik:$(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $MAN_KDB Repositories/GitHub/token)@github.com/HerrMAzik/pass.git $HOME/repo/pass
+    git clone https://HerrMAzik:$(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM Repositories/GitHub/token)@github.com/HerrMAzik/pass.git $HOME/repo/pass
     sh -c 'cd $HOME/repo/pass; git remote set-url origin git@github.com:HerrMAzik/pass.git'
 fi
 [ ! -L $HOME/.password-store ] && ln -s $HOME/repo/pass $HOME/.password-store
@@ -106,7 +83,7 @@ fi
 if [ ! -d $HOME/repo/settings ]; then
     test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
 
-    git clone https://HerrMAzik:$(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $MAN_KDB Repositories/GitHub/token)@github.com/HerrMAzik/settings.git $HOME/repo/settings
+    git clone https://HerrMAzik:$(yes $passphrase | keepassxc-cli show -q -a Password -s -k $HOME/.sanctum.sanctorum $SANCTUM_SANCTORUM Repositories/GitHub/token)@github.com/HerrMAzik/settings.git $HOME/repo/settings
     sh -c 'cd $HOME/repo/settings; git remote set-url origin git@github.com:HerrMAzik/settings.git'
 fi
 
---
+++ sanctum.sanctorum.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+if [ ! -f $SANCTUM_SANCTORUM ]; then
+    while : ; do
+        test -z $passphrase && echo 'enter kdbx password:' && read -ers passphrase
+        link=$(echo 'jA0ECQMCItwerSoXDA3o0lYBs5LFklMXCSTWb9FFsTdqXcPlVoFAHK6q6dZc7OF04lhUcFiKpCgpUgSde+CuPhSqIcGzdD7dyizdgu4aA91oX+QdhN7KLdiJSp7YJ7QyQTbYY2Smaw==' | base64 --decode | gpg --decrypt --batch --quiet --passphrase "$passphrase")
+        [ $? -eq 0 ] && echo $link && break
+        unset passphrase
+    done
+
+    resp=$(curl -sSL "https://cloud-api.yandex.net/v1/disk/public/resources?public_key=$link")
+    error=$(echo $resp | jq -r .error)
+    [ "$error" != "null" ] && echo "Error: $error"
+    link=$(echo $resp | jq -r .file)
+    [ "$link" == "null" ] || [ $? -ne 0 ] && link=$(echo 'jA0ECQMCXOfwQMLRH93p0lkBXcQzC1SyWhccifyEn1QGeU7VS7Q7+aJLuI5iP7EiOBkKGaMteZ68aF6bbuVGjjZLw4L/BB3br6CK+4yF0/0nRREXoQyEee1AVoE1OaDG/kqq7oa/QFy3Kg==' | base64 --decode | gpg --decrypt --batch --quiet --passphrase "$passphrase")
+
+    curl -sSL --output /tmp/kdbx $link
+    echo "kdbx has been downloaded"
+
+    while : ; do
+        echo 'enter password for kdb archive:' && read -ers z
+        gpg --passphrase "$z" --batch --quiet --decrypt /tmp/kdbx | xz -d > $SANCTUM_SANCTORUM
+        [ $? -eq 0 ] && break
+    rm -rf $SANCTUM_SANCTORUM
+    done
+fi
